3c21317a2ddd24b5d8abe75f03b2143e97ceaf34
Pavel <pishchalaups@belhard.com>
2019-12-23 16:38:17 +0300

    add action

---
+++ src/app/redux/cars.action.ts
@@ -0,0 +1,14 @@
+import { Action } from '@ngrx/store';
+
+import { Car } from '../car.model';
+
+// tslint:disable-next-line: no-namespace
+export namespace CAR_ACTION {
+    export const ADD_CAR = 'ADD_CAR';
+}
+
+export class AddCar implements Action {
+    readonly type = CAR_ACTION.ADD_CAR;
+
+    constructor(public payload: Car) {}
+}
